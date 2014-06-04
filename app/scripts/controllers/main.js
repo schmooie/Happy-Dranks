6504cfe566461e7707507c5753b27c4c06987361
Sam Lau <slau234@gmail.com>
2014-06-04 19:36:43 -0400

    infoWindow working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,40 +3,36 @@
 angular.module('meanHappyHourApp')
   .controller('MainCtrl', function ($scope, $http) {
 		var location;
-		var barsArr = [];
+		$scope.bars = [];
 
 		var onMarkerClicked = function (marker) {
-			// _.each($scope.map.markers, function(mker){
-			// 	mker.showWindow = false;
-			// });
 			marker.showWindow = true;
 			$scope.$apply();
 		};
 
 		var makeMarkers = function (arr) {
 			  for (var i = arr.length -1; 0 <= i; i--) {
-      	var bar = arr[i];
-      	var marker = {
-      		coords:{
-      			latitude: bar.latitude,
-      			longitude: bar.longitude},
-      		text: bar.name,
-      		showWindow: false,
-      		id: $scope.map.markers.length + 1
-      	};
-      	marker.closeClick = function () {
-      		marker.showWindow = false;
-      		$scope.$apply();
-      	};
-      	marker.onClicked = function () {
-      		onMarkerClicked(marker);
-      	};
-      	marker.windowOptions = {
-      		pixelOffset: new google.maps.Size(0,5),
-      		disableAutoPan: true,
-      		boxClass: 'custom-info-window'
-      	};
-      	$scope.map.markers.push(marker);
+	      	var bar = arr[i];
+	      	var marker = {
+	      		coords:{
+	      			latitude: bar.latitude,
+	      			longitude: bar.longitude},
+	      		text: bar.name,
+	      		showWindow: false
+	      		};
+	      	marker.closeClick = function () {
+	      		marker.showWindow = false;
+	      		$scope.$apply();
+	      	};
+	      	marker.onClicked = function () {
+	      		onMarkerClicked(marker);
+	      	};
+	      	marker.windowOptions = {
+	      		pixelOffset: new google.maps.Size(0,5),
+	      		disableAutoPan: true,
+	      		boxClass: 'custom-info-window'
+	      	};
+	      	$scope.bars[i].marker = marker;
       }
 		};
 
@@ -52,8 +48,8 @@ angular.module('meanHappyHourApp')
 		};
 
     $http.get('/api/bars/search?happyHour=Yes').success(function(bars) {
-    		barsArr = bars.slice(0,50);
-    		makeMarkers(barsArr);
+    		$scope.bars = bars.slice(0,50);
+    		makeMarkers($scope.bars);
     });
 
 
@@ -66,9 +62,9 @@ angular.module('meanHappyHourApp')
 			$scope.map.center = location;
 			$http.get('/api/bars/nearest?' + 'longitude=' + $scope.map.center.longitude + '&latitude=' + $scope.map.center.latitude)
 			.success(function(bars){
-				barsArr = [];
-				barsArr = bars;
-				makeMarkers(barsArr);
+				$scope.bars = [];
+				$scope.bars = bars;
+				makeMarkers($scope.bars);
 			});
 		};
 
