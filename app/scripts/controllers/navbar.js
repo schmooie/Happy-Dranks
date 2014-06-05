517f73d6dac630f6d01ffb7a57b0243c40a4bd36
Sam Lau <slau234@gmail.com>
2014-06-05 12:17:09 -0400

    Some styling.

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -3,10 +3,10 @@
 angular.module('meanHappyHourApp')
   .controller('NavbarCtrl', function ($scope, $location) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'HOME',
       'link': '/'
     }];
-    
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
