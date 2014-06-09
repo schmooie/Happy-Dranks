b90f669f5a6324ccfb09f29d32eedf79ebd01771
Sam Lau <slau234@gmail.com>
2014-06-08 23:59:54 -0400

    Using Express-Restify-Mongoose for API layer.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('meanHappyHourApp')
-  .controller('MainCtrl', function (mapFuncs, $scope, $http) {
-		var myLocation;
+  .controller('MainCtrl', function (mapFuncs, $scope, $http, $rootScope) {
 		$scope.bars = [];
 		$scope.iconClass = 'fa fa-frown-o fa-5x';
     $scope.map = {
@@ -20,26 +19,34 @@ angular.module('meanHappyHourApp')
 	    }
 		};
 
-    $http.get('/api/bars/search?happyHour=Yes').success(function(bars) {
+    $http.get('/api/v1/Bars?happyHour=Yes').success(function(bars) {
     		$scope.bars = mapFuncs.makeMarkers(bars.slice(0, 24));
     });
 
 		$scope.findNear = function () {
+			$scope.map.center = $rootScope.myLocation;
+			var query={
+				happyHour: 'Yes',
+				coords: {
+					$near: [$scope.map.center.longitude, $scope.map.center.latitude],
+					$maxDistance: 0.0145
+				}
+			};
 			$scope.map.zoom = 14;
-			$scope.map.center = myLocation;
-			$http.get('/api/bars/nearest?' + 'longitude=' + $scope.map.center.longitude + '&latitude=' + $scope.map.center.latitude)
+			$http.get('/api/v1/Bars?query=' + JSON.stringify(query))
 			.success(function(bars){
 				$scope.iconClass = 'fa fa-smile-o fa-5x';
 				$scope.numNearby = bars.length < 6 ? bars.length : 6;
 				$scope.bars = $scope.bars.concat(mapFuncs.makeMarkers(bars.slice(0, $scope.numNearby)));
 				setTimeout(function(){
 					$scope.nearbyBars = true;
-				}, 300);
+					$scope.$apply();
+				}, 450);
 			});
 		};
 
 		var onSuccess = function (position) {
-			myLocation = {
+			$rootScope.myLocation = {
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude
 			};
--- app/scripts/controllers/searchpage.js
+++ app/scripts/controllers/searchpage.js
@@ -1,8 +1,40 @@
 'use strict';
 
 angular.module('meanHappyHourApp')
-  .controller('SearchPageCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+  .controller('SearchPageCtrl', function (mapFuncs, $scope, $http, $rootScope) {
+		$scope.bars = [];
+		$scope.iconClass = 'fa fa-frown-o fa-3x';
+
+    $scope.map = {
+	    center: {
+        latitude: 40.7311227,
+        longitude: -73.9498119
+	    },
+	    zoom: 12,
+	    refresh: 'true',
+	    markers: [],
+	    control: {},
+	    options: {
+	    	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	    	styles: mapFuncs.style
+	    }
+		};
+
+		// error messages
+		$scope.foundBars = false;
+		$scope.notSearched = true;
+		$scope.notFound = false;
+
+
+
+    $scope.search = function () {
+    	$http.get('/api/bars/search?')
+    	.success(function(bars){
+    			$scope.bars = mapFuncs.makeMarkers(bars);
+    			$scope.foundBars = true;
+  		})
+  		.error(function(){
+  			$scope.notFound = true;
+  		});
+    };
   });
